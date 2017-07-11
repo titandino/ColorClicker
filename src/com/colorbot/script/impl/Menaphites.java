bdd211455aceea4cfca3d8e66611074426e3652b
Trenton Kress <trenton.kress@gmail.com>
2017-07-10 21:15:18 -0700

    Incorporate clicks to menaphite script

--- src/com/colorbot/script/impl/Menaphites.java
+++ src/com/colorbot/script/impl/Menaphites.java
@@ -1,6 +1,7 @@
 package com.colorbot.script.impl;
 
 import java.awt.Color;
+import java.awt.Point;
 
 import com.colorbot.bot.Bot;
 import com.colorbot.script.Script;
@@ -12,8 +13,13 @@ public class Menaphites extends Script {
 		try {
 			Bot.MOUSE_SPEED = Bot.random(2, 5);
 			if (Bot.getHealthPercent() > 20) {
-				Bot.moveMouse(new Color(64, 201, 243), new Color(90, 46, 210), 10, 20, 30, 10);
-				Bot.click();
+				Point p1 = Bot.getPointWithColorCombo(new Color(64, 201, 243), new Color(90, 46, 210), 10, 20);
+				if (p1 == null)
+					p1 = Bot.getPointWithColorCombo(new Color(64, 201, 243), new Color(90, 46, 210), 10, 20);
+				if (p1 != null) {
+					Bot.moveMouse(p1, 30, 10);
+					Bot.click();
+				}
 			} else {
 				Bot.pressKey('R');
 			}
