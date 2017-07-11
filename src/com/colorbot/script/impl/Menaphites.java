147f4ab680a3a46bd1735a5745f94dc4864d1391
Trenton Kress <trenton.kress@gmail.com>
2017-07-10 21:19:45 -0700

    :ok_hand: menaphite script

--- src/com/colorbot/script/impl/Menaphites.java
+++ src/com/colorbot/script/impl/Menaphites.java
@@ -15,7 +15,7 @@ public class Menaphites extends Script {
 			if (Bot.getHealthPercent() > 20) {
 				Point p1 = Bot.getPointWithColorCombo(new Color(64, 201, 243), new Color(90, 46, 210), 10, 20);
 				if (p1 == null)
-					p1 = Bot.getPointWithColorCombo(new Color(64, 201, 243), new Color(90, 46, 210), 10, 20);
+					p1 = Bot.getPointWithColorCombo(new Color(64, 201, 243), new Color(194, 82, 65), 10, 20);
 				if (p1 != null) {
 					Bot.moveMouse(p1, 30, 10);
 					Bot.click();
